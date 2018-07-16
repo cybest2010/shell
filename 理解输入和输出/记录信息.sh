1b2a5eb50f9ac0fc0cd3ab4aba840b1e06d3d9f8
cybest2010 <971959755@qq.com>
2018-07-16 16:15:30 +0800

    Update 记录信息.sh

--- 理解输入和输出/记录信息.sh
+++ 理解输入和输出/记录信息.sh
@@ -3,6 +3,7 @@
 # using the tee command for logging
 #将输入一边发送到STDOUT，一边发送到日志文件
 tempfile=test
+#生成test文件，并将结果标准输出的同时写入test文件
 echo "This is the start of the test" | tee $tempfile
 echo "This is the second line of the test" | tee -a $tempfile
 echo "This is the end line of the test" | tee -a $tempfile
